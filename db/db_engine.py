6038090cff52e5347474e236c174d19f1f83bab3
Blendi <dronramone@mail.ru>
2023-10-12 19:47:01 +0400

    Create DB-engine

--- db/db_engine.py
+++ db/db_engine.py
@@ -1,2 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import create_engine
 
-class Settings
+
+class Settings(BaseSettings):
+
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+    POSTGRES_HOST_AUTH_METHOD: str
+    DB_HOST: str
+    DB_PORT: int
+
+    @property
+    def create_engine(self):
+        engine = create_engine(
+            f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}"
+            f":{self.DB_PORT}/{self.POSTGRES_DB}",
+            echo=True,
+        )
+        return engine
+
+    model_config = SettingsConfigDict(env_file='connect_db.env')
+
+
+settings = Settings()
